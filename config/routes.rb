b9d1d1e7c8b0594259e772fadb71e3bf0b8b8b6d
olawale-o <omoogunolawale@gmail.com>
2022-05-24 16:42:04 +0100

    Add private route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   scope :v1, defaults: { format: :json } do
     mount_devise_token_auth_for 'User', at: 'auth'
   end
+
+  namespace :v1, defaults: { format: :json } do
+    get '/private', to: 'private#index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
